bd823d9008a60e1d98b86bdb3bc7fe5edad60734
maickol nuñez <maickol.nunez@sansano.usm.cl>
2022-09-30 21:15:36 -0300

    Upload Files

---
+++ js/video.js
@@ -0,0 +1,44 @@
+var myVideoP = document.getElementById("videoP"); 
+
+function playPauseP() { 
+  if (myVideoP.paused) 
+    myVideoP.play(); 
+  else 
+    myVideoP.pause();
+} 
+
+var myVideoQ = document.getElementById("videoQ"); 
+
+function playPauseQ() { 
+  if (myVideoQ.paused) 
+    myVideoQ.play(); 
+  else 
+    myVideoQ.pause();
+} 
+
+var myVideoW = document.getElementById("videoW"); 
+
+function playPauseW() { 
+  if (myVideoW.paused) 
+    myVideoW.play(); 
+  else 
+    myVideoW.pause();
+} 
+
+var myVideoE = document.getElementById("videoE"); 
+
+function playPauseE() { 
+  if (myVideoE.paused) 
+    myVideoE.play(); 
+  else 
+    myVideoE.pause();
+} 
+
+var myVideoR = document.getElementById("videoR"); 
+
+function playPauseR() { 
+  if (myVideoR.paused) 
+    myVideoR.play(); 
+  else 
+    myVideoR.pause();
+} 
